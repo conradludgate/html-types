efebb46fa7025a36762767cf4204b3bdeafb74da
Conrad Ludgate <conrad.ludgate@truelayer.com>
2022-08-08 22:47:12 +0100

    improve

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 use std::{borrow::Cow, fmt::Write, ops::Deref};
 
-// pub enum DynList<'a, T: ?Sized> {
-//     Owned(Box<[Box<T>]>),
-//     Borrowed(&'a [&'a T]),
-// }
 pub type DynList<'a, T> = DynElem<'a, [DynElem<'a, T>]>;
 pub enum DynElem<'a, T: ?Sized + 'a> {
     Owned(Box<T>),
@@ -38,8 +34,61 @@ pub struct Formatter<'a> {
     buf: &'a mut (dyn Write + 'a),
 }
 
+type Result<T = (), E = std::fmt::Error> = std::result::Result<T, E>;
+
+impl<'a> Formatter<'a> {
+    fn render_tabs(&mut self) -> Result {
+        for _ in 0..self.depth {
+            self.buf.write_str("  ")?;
+        }
+        Ok(())
+    }
+    pub fn start(&mut self, element: &'static str) -> Result<ElementFmt<'_, 'a>> {
+        self.render_tabs()?;
+        write!(self.buf, "<{element}")?;
+        self.depth += 1;
+        Ok(ElementFmt {
+            fmt: self,
+            element,
+            children: 0,
+        })
+    }
+}
+
+pub struct ElementFmt<'a, 'b> {
+    fmt: &'a mut Formatter<'b>,
+    element: &'static str,
+    children: usize,
+}
+
+impl Drop for ElementFmt<'_, '_> {
+    fn drop(&mut self) {
+        self.fmt.depth -= 1;
+        // close the tag approriately
+        if self.children > 0 {
+            self.fmt.render_tabs().unwrap();
+            self.fmt.buf.write_str("</").unwrap();
+            self.fmt.buf.write_str(self.element).unwrap();
+            self.fmt.buf.write_str(">\n").unwrap();
+        } else {
+            self.fmt.buf.write_str("/>\n").unwrap();
+        }
+    }
+}
+
+impl ElementFmt<'_, '_> {
+    pub fn render_child(mut self, child: &dyn Element) -> Result<Self> {
+        if self.children == 0 {
+            self.fmt.buf.write_str(">\n").unwrap();
+        }
+        self.children += 1;
+        child.render(self.fmt)?;
+        Ok(self)
+    }
+}
+
 pub trait ElementExt: Element {
-    fn render_to_string(&self) -> Result<String, std::fmt::Error> {
+    fn render_to_string(&self) -> Result<String> {
         let mut buf = String::new();
         self.render(&mut Formatter {
             depth: 0,
@@ -51,48 +100,8 @@ pub trait ElementExt: Element {
 impl<E: Element> ElementExt for E {}
 
 pub trait Element {
-    fn render(&self, fmt: &mut Formatter) -> std::fmt::Result;
+    fn render(&self, fmt: &mut Formatter) -> Result;
 }
-
-#[macro_export]
-macro_rules! dyn_list {
-    ($($v:expr),* $(,)?) => {
-        [$($v),*]
-    };
-}
-
-// #[macro_export]
-// #[doc(hidden)]
-// macro_rules! inner_dyn_list {
-//     ($($v:expr),* ; box($b:expr) $(, $r:expr)*) => {
-//         inner_dyn_list!(
-//             $($v,)*
-//             $crate::owned!($b);
-//             $($r),*
-//         )
-//     };
-//     ($($v:expr),* ; $b:expr $(, $r:expr)*) => {
-//         inner_dyn_list!(
-//             $($v,)*
-//             borrow!($b);
-//             $($r),*
-//         )
-//     };
-//     ($($v:expr),* ;) => {
-//         [$($v),*]
-//     };
-// }
-
-// #[macro_export]
-// macro_rules! box_dyn_list {
-//     ($($v:expr),* $(,)?) => {
-//         $crate::DynList::Owned(
-//             vec![$(
-//                 Box::new($v) as Box<_>
-//             ),*].into_boxed_slice()
-//         )
-//     };
-// }
 
 pub struct Body<'a> {
     children: DynList<'a, dyn Element + 'a>,
@@ -103,46 +112,28 @@ pub struct Div<'a> {
 pub struct Text<'a>(pub Cow<'a, str>);
 
 impl<T: AsRef<str> + ?Sized> Element for T {
-    fn render(&self, fmt: &mut Formatter) -> std::fmt::Result {
-        for _ in 0..fmt.depth {
-            fmt.buf.write_char('\t')?;
-        }
-        writeln!(fmt.buf, "{}", self.as_ref())
+    fn render(&self, fmt: &mut Formatter) -> Result {
+        fmt.render_tabs()?;
+        fmt.buf.write_str(self.as_ref())?;
+        fmt.buf.write_char('\n')
     }
 }
 
 impl Element for Body<'_> {
-    fn render(&self, fmt: &mut Formatter) -> std::fmt::Result {
-        for _ in 0..fmt.depth {
-            fmt.buf.write_char('\t')?;
-        }
-        fmt.buf.write_str("<body>\n")?;
-        fmt.depth += 1;
-        for child in &*self.children {
-            child.render(&mut *fmt)?;
-        }
-        fmt.depth -= 1;
-        for _ in 0..fmt.depth {
-            fmt.buf.write_char('\t')?;
-        }
-        fmt.buf.write_str("</body>\n")
+    fn render(&self, fmt: &mut Formatter) -> Result {
+        self.children
+            .iter()
+            .try_fold(fmt.start("body")?, |f, e| f.render_child(&**e))?;
+        Ok(())
     }
 }
+
 impl Element for Div<'_> {
-    fn render(&self, fmt: &mut Formatter) -> std::fmt::Result {
-        for _ in 0..fmt.depth {
-            fmt.buf.write_char('\t')?;
-        }
-        fmt.buf.write_str("<div>\n")?;
-        fmt.depth += 1;
-        for child in &*self.children {
-            child.render(&mut *fmt)?;
-        }
-        fmt.depth -= 1;
-        for _ in 0..fmt.depth {
-            fmt.buf.write_char('\t')?;
-        }
-        fmt.buf.write_str("</div>\n")
+    fn render(&self, fmt: &mut Formatter) -> Result {
+        self.children
+            .iter()
+            .try_fold(fmt.start("div")?, |f, e| f.render_child(&**e))?;
+        Ok(())
     }
 }
 
@@ -155,6 +146,7 @@ fn foo(name: &str) -> String {
             b!(Div {
                 children: b!([b!("foo"), b!("bar")])
             }),
+            b!(Div { children: b!([]) }),
             b!("baz"),
         ]),
     }
@@ -166,12 +158,3 @@ fn foo(name: &str) -> String {
 fn test() {
     println!("{}", foo("hello"))
 }
-
-// fn name1(name: &str) -> DynElem<'_, dyn Element> {
-//     // borrow!(name)
-//     DynElem::Owned { 0: Box::new(name) }
-// }
-
-// fn name(name: &str) -> DynList<'_, dyn Element> {
-//     DynList::Borrowed { 0: &[name1(name)] }
-// }
